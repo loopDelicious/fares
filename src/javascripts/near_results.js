f4de72a2ab1b4833b9b2ee5e2d56f6429c5735d8
Joyce Lin <joycelin79@gmail.com>
2016-12-16 18:33:22 -0800

    formatting nearby drivers results

--- src/javascripts/near_results.js
+++ src/javascripts/near_results.js
@@ -7,14 +7,25 @@ class Result extends Component {
 
         var results = this.props.results.map( (result, i) => {
 
-            console.log(result);
+            var locations = result.drivers.map( (cluster) => {
+                return cluster.locations[0];
+            }); // array of locations returning 1 car from each location cluster
+
+            var coordinates = locations.map( (location) => {
+               return (
+                   <h5>{location.lat}, {location.lng}</h5>
+               )
+            });
+
+            var rideTypes = result.ride_type.split('_').join(" ");
 
             return (
                 <li key={i}>
                     <div className="result clearfix">
                         <i className="fa fa-car" />
                         <div className="result-data">
-                            <h4>{result.ride_type}: </h4>
+                            <h4 className="ride-type">{rideTypes}</h4>
+                            {coordinates}
                         </div>
                     </div>
 
